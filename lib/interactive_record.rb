d45ab4d0218013c534a747f003527fc743fe5178
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-29 18:14:19 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,9 +19,7 @@ class InteractiveRecord
   end
 
   def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
+    options.each { |property, value| self.send("#{property}=", value) }
   end
 
   def save
