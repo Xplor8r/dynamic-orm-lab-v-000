003ca4d5eee5f209a15b89f9b0b72e8ae445566b
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-29 18:45:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,7 @@ class InteractiveRecord
   def self.find_by(attribute)
     attribute_value = attribute.values
     sql_value = attribute_value.class
-    if attribute_value.class.is? Integer
+    if attribute_value.class.is_a? Integer
       value
     else
       "'#{value}'"
